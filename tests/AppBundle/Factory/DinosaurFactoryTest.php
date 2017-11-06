60427e6ac6f0b7542d41ebfe5c875a223e1bff8f
Ryan Weaver <ryan@thatsquality.com>
2017-11-06 11:58:09 -0500

    Post 04b-skip-incomplete

--- tests/AppBundle/Factory/DinosaurFactoryTest.php
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -27,4 +27,19 @@ class DinosaurFactoryTest extends TestCase
         $this->assertSame('Velociraptor', $dinosaur->getGenus());
         $this->assertSame(5, $dinosaur->getLength());
     }
+
+    public function testItGrowsATriceratops()
+    {
+        $this->markTestIncomplete('Waiting for confirmation from GenLab');
+    }
+
+    public function testItGrowsABabyVelociraptor()
+    {
+        if (!class_exists('Nanny')) {
+            $this->markTestSkipped('There is nobody to watch the baby');
+        }
+
+        $dinosaur = $this->factory->growVelociraptor(1);
+        $this->assertSame(1, $dinosaur->getLength());
+    }
 }
